c2f3a9a6856a5d83309238e05d69d227bc93dd0a
Josef Zacek <info@josefzacek.com>
2015-03-25 21:09:41 +0000

    basic functionality working

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="trumbowyg.min.css">
+</head>
+<body>
+  
+<h1>Trumbowyg</h1>
+   
+   <div id="trumbowyg-demo"></div>
+     
+   
+
+    
+</body>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+
+<script src="trumbowyg.min.js"></script>
+
+<script>
+    $( document ).ready(function() {
+         $('#trumbowyg-demo').trumbowyg();
+    });
+</script>
+
+
+</html>
